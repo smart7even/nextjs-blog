3771e6609cbc29bdcec5766edc016f6e15094a84
smart7even <smart7even@yandex.ru>
2022-12-25 16:41:01 +0300

    feat: add authorized page

--- pages/authorized.js
+++ pages/authorized.js
@@ -2,20 +2,25 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Script from 'next/script'
+import { useRouter } from 'next/router'
+
 
 export default function Home() {
+    const router = useRouter()
+    const { id, first_name, last_name, username, photo_url, auth_date, hash } = router.query
+
     return (
         <Layout home>
             <Head>
                 <title>{siteTitle}</title>
             </Head>
-            <Script async src="https://telegram.org/js/telegram-widget.js?21" data-telegram-login="roadmapik_bot" data-size="large" data-auth-url="https://roadmapik.com/authorized" data-request-access="write"></Script>
             <section className={utilStyles.headingMd}>
                 <p>Software engineer, donut lover, SPbSEU student</p>
                 <p>
                     (This is a sample website - you’ll be building a site like this on{' '}
                     <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
                 </p>
+                <p>You are logged in as {first_name} {last_name}</p>
             </section>
         </Layout>
     );
