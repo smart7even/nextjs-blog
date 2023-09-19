7bbe2367fb9ba9247d18f70af20cfd45217e75fb
Oleg <upliftingpark@gmail.com>
2023-09-19 21:58:39 +0300

    feat: change page title and icon

--- components/layout.js
+++ components/layout.js
@@ -5,16 +5,16 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Oleg Magomedov';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Oleg Magomedov';
 
 export default function Layout({ children, home }) {
     return (
         <div className={styles.container}>
             <Head>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/icon.svg" />
                 <meta
                     name="description"
-                    content="Learn how to build a personal website using Next.js"
+                    content="Personal website built with Next.js"
                 />
                 <meta
                     property="og:image"
