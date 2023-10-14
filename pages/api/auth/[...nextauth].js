6c1c37b29c63b41a86b787cf6f3c018e4f8d8be6
Oleg <upliftingpark@gmail.com>
2023-10-14 21:01:19 +0300

    feat: persist auth to database

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import { PrismaClient } from "@prisma/client"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+
+const prisma = new PrismaClient()
 
 export const authOptions = {
     // Configure one or more authentication providers
@@ -15,5 +19,6 @@ export const authOptions = {
         })
         // ...add more providers here
     ],
+    adapter: PrismaAdapter(prisma),
 }
 export default NextAuth(authOptions)
