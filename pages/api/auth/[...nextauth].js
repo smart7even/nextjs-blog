a19497110463b9c004f5b71ab140b701dfc39941
Oleg <upliftingpark@gmail.com>
2023-10-14 21:07:34 +0300

    fix: import prisma from lib/prisma

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,8 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
-import { PrismaClient } from "@prisma/client"
+import { prisma } from "../../../lib/prisma"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-
-const prisma = new PrismaClient()
 
 export const authOptions = {
     // Configure one or more authentication providers
